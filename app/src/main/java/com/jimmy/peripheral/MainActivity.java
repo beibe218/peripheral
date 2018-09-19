85373fe99349948c3931629550f9b72f2b2157bb
Gijimmy <Gibeibe218@163.com>
2018-09-19 18:43:22 +0800

    first commit

--- app/src/main/java/com/jimmy/peripheral/MainActivity.java
+++ app/src/main/java/com/jimmy/peripheral/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void toSerialPortAct(View view) {
-        Intent intent = new Intent(this, SerialPortActivity.class);
+        Intent intent = new Intent(this, SerialPortReadActivity.class);
         startActivity(intent);
     }
 
---
+++ app/src/main/java/com/jimmy/peripheral/SerialPortReadActivity.java
@@ -0,0 +1,93 @@
+package com.jimmy.peripheral;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.jimmy.serial.ReadCallback;
+import com.jimmy.serial.SerialPortRead;
+import com.jimmy.serial.utils.SerialPortFinder;
+
+public class SerialPortReadActivity extends AppCompatActivity {
+
+    private String[] allDevicesPath;
+    private SerialPortRead serialPortRead;
+    private TextView pathTv;
+    private TextView baudRateTv;
+    private TextView readResultTv;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_serial_port_read);
+        getSupportActionBar().setTitle(R.string.serial_port);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        SerialPortFinder serialPortFinder = new SerialPortFinder();
+        allDevicesPath = serialPortFinder.getAllDevicesPath();
+        serialPortRead = new SerialPortRead();
+
+        pathTv = findViewById(R.id.path_tv);
+        baudRateTv = findViewById(R.id.baud_rate_tv);
+        readResultTv = findViewById(R.id.read_result_tv);
+    }
+
+    private ReadCallback readCallback = new ReadCallback() {
+        @Override
+        public void onReading(String s) {
+            readResultTv.setText(s);
+        }
+    };
+
+    public void showSerialPortPathDialog(View view) {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.serial_port_path)
+                .setItems(allDevicesPath, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        pathTv.setText(allDevicesPath[which]);
+                    }
+                })
+                .show();
+    }
+
+    public void showBaudRateDialog(View view) {
+        final String[] baudRates = new String[]{"2400", "9600"};
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.serial_port_path)
+                .setItems(baudRates, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        baudRateTv.setText(baudRates[which]);
+                    }
+                })
+                .show();
+    }
+
+    private void open() {
+        String path = pathTv.getText().toString();
+        String baudRate = baudRateTv.getText().toString();
+        if (TextUtils.isEmpty(baudRate) || TextUtils.isEmpty(path)) {
+            return;
+        }
+        serialPortRead.open(path, Integer.parseInt(baudRate), readCallback);
+    }
+
+    public void testConnect(View view) {
+        open();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
