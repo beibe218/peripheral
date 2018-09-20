1963e012ded390b373ad7ef44d8f72ddcc44a6b5
Gijimmy <Gibeibe218@163.com>
2018-09-20 20:35:39 +0800

    网了请求调试完成

--- print/src/androidTest/java/com/jimmy/data/remote/JApiTest.java
+++ print/src/androidTest/java/com/jimmy/data/remote/JApiTest.java
@@ -2,9 +2,12 @@ package com.jimmy.data.remote;
 
 import android.util.Log;
 
+import com.jimmy.data.db.model.Item;
+
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -18,19 +21,19 @@ public class JApiTest {
     @Test
     public void testGetItems() {
         JApi jApi = new JApi();
-        Call<ResponseBody> call = jApi.getItems("10108481", "888", 300, "", "");
-        call.enqueue(new Callback<ResponseBody>() {
+        Call<HttpResult<Item>> call = jApi.getItems("10108481", "888", 300, "", "");
+        call.enqueue(new Callback<HttpResult<Item>>() {
             @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                String s = response.body().toString();
-                Log.d(TAG, s);
+            public void onResponse(Call<HttpResult<Item>> call, Response<HttpResult<Item>> response) {
+                HttpResult<Item> body = response.body();
+                List<Item> items = body.getList();
+                for (Item item : items) {
+                    Log.d(TAG, item.getName());
+                }
             }
 
             @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                for (int i = 0; i <= 1; i++) {
-                    Log.d(TAG, "i = " + i);
-                }
+            public void onFailure(Call<HttpResult<Item>> call, Throwable t) {
                 t.printStackTrace();
             }
         });
